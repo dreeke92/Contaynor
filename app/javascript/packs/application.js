57d3efbd72ae64fbdf8e5a0fea53abea57c6391e
Andreas Van Assche <andreasava@hotmail.com>
2019-09-08 17:34:52 +0200

    Set up template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+
+import '../template/jquery';
+import '../template/dropotron';
+import '../template/scrollex';
+import '../template/scrolly';
+import '../template/browser';
+import '../template/breakpoints';
+import '../template/util';
+import '../template/main';
+
+
+
+
