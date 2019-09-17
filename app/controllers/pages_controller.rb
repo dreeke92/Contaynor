9241dab7dcdbe88925aea7427caac294204c3c30
Andreas Van Assche <andreasava@hotmail.com>
2019-09-17 20:29:58 +0200

    Further progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :elements, :generic]
+  skip_before_action :authenticate_user!, only: [:home, :elements, :generic, :landing]
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'generic', to: "pages#generic"
   get 'elements', to: "pages#elements"
+  get 'home', to: "pages#home"
 end
